e0c9c773a51d7dfb5487061294b164db28d154c3
Martin Vejmelka <vejmelkam@gmail.com>
2014-04-14 14:27:13 -0600

    added error computations for wrf forecasting for arbitrary ranges

--- src/compute_tsm_wrf_errors.m
+++ src/compute_tsm_wrf_errors.m
@@ -2,37 +2,33 @@
 %
 % Evaluates the MAPE,mean error and RMSE for the WRF forecast test.
 %
-% Synopsis: [sns,errs,aerrs,rmse] = compute_tsm_wrf_errors(dirname,stop_da_at)
+% Synopsis: [sns,errs,aerrs,rmse] = compute_tsm_wrf_errors(fname,rng)
 %
 
 function [sns,errs,aerrs,rmse,Nt] = compute_tsm_wrf_errors(fname,rng)
 
     d = load(fname);
     Nst = length(d.sds);
+    Nr = length(rng);
     
-    errs = zeros(Nst,1);
-    aerrs = zeros(Nst,1);
-    rmse = zeros(Nst,1);
-    sns = cell(Nst,1);
-    Nt = zeros(Nst,1);
+    errs = zeros(Nr,Nst);
+    aerrs = zeros(Nr,Nst);
+    rmse = zeros(Nr,Nst);
+    sns = cell(Nst);
+    Nt = zeros(Nr,Nst);
     
     for i=1:Nst
+        sns{i} = d.sds{i}.stid;
         fm10 = d.fm10_tgt(:,i);
         pred = d.fm10_model(:,i,2);
-        valid = false(size(fm10));
-        valid(rng) = true;
-        valid(~isfinite(fm10)) = false;
-        N = sum(valid);
-        if(N > 0)
-            Nt(i) = N;
-            sns{i} = d.sds{i}.stid;
-            errs(i) = sum(pred(valid) - fm10(valid));
-            aerrs(i) = norm(pred(valid) - fm10(valid),1);
-            rmse(i) = norm(pred(valid) - fm10(valid),2);
+        for t=1:Nr
+            r = rng(t);
+            if(isfinite(fm10(r)))
+                Nt(t,i) = Nt(t,i)+1;
+                errs(t,i) = pred(r) - fm10(r);
+                aerrs(t,i) = abs(pred(r) - fm10(r));
+                rmse(t,i) = (pred(r) - fm10(r))^2;
+            end
         end
     end
-        
-    
-    
-    
     
